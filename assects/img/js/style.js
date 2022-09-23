f3235f6f1897edc9ee62f4deb7e650acf4c84e74
Sithumameesha <sithumameesha76@gmail.com>
2022-09-23 14:12:00 +0530

    First Commit

---
+++ assects/img/js/style.js
@@ -0,0 +1,24 @@
+var chara =document.getElementById("cha");
+var block =document.getElementById("block");
+function jump(){
+    if (chara.classList != "animate"){
+        chara.classList.add("animate");
+    }
+
+    setTimeout(function (){
+        chara.classList.remove("animate")
+    },500);
+}
+var dead = setInterval(function (){
+   var chatop =
+       parseInt(window.getComputedStyle(chara).getPropertyValue("top"));
+
+    var chaleft =
+        parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    if (chaleft<20 && chaleft>0 &&
+    chatop>=100){
+        block.style.animation = "none";
+        block.style.display ="none";
+alert("You Lose");
+    }
+},10);
